302ed5de4fd2877069e756548c741ca8b932be57
asnowpro <142566218+asnowpro@users.noreply.github.com>
2023-08-18 11:47:37 +0530

    Update streamlit_app.py
    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,10 @@ streamlit.title("First Streamlit App")
 streamlit.text("try adding some more text")
 streamlit.text("It is wonderful!")
 streamlit.text("Make a great day!")
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
