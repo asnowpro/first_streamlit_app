05406b7a3a5f9ef8db52c57b36b6a41acb83a2f7
asnowpro <142566218+asnowpro@users.noreply.github.com>
2023-08-20 22:13:50 +0530

    added break point to troubleshoot using streamlit.stop command

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,6 +56,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Print it as a table in the UI
 streamlit.dataframe(fruityvice_normalized)
 
+#do not run anything past here while we troubleshoot
+streamli.stop()
+
 #import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
