0970fa93e2b0deced0f24fa7815f4ef17e894599
asnowpro <142566218+asnowpro@users.noreply.github.com>
2023-08-20 22:23:43 +0530

    try except with nested if else

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,18 +43,34 @@ streamlit.dataframe(fruits_to_show)
 # new section to display fruityvice api repsonse
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+# fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+# streamlit.write('The user entered ', fruit_choice)
+
+# adding try-except with nested if-else
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error('please select a fruit to get information.')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # normalize the json reponse 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # Print it as a table in the UI
+    streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+  streamlit.error()
+  
 
 # import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response)
-#streamlit.text(fruityvice_response.json())
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# streamlit.text(fruityvice_response)
+# streamlit.text(fruityvice_response.json())
 
 # normalize the json reponse 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Print it as a table in the UI
-streamlit.dataframe(fruityvice_normalized)
+# streamlit.dataframe(fruityvice_normalized)
 
 #do not run anything past here while we troubleshoot
 streamlit.stop()
